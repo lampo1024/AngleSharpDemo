daf4426898c9f06887c9e45d040dd6de6e153104
Rector <lampo1024@gmail.com>
2017-10-23 18:04:45 +0800

    Init project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using AngleSharp.Parser.Html;
+using System;
+
+namespace AngleSharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a (re-usable) parser front-end
+            var parser = new HtmlParser();
+            //Source to be pared
+            var source = "<h1>Some example source</h1><p>This is a paragraph element <img src=\"http://google.com\"></p>";
+            //Parse source to document
+            var document = parser.Parse(source);
+            //Do something with document like the following
+
+            var elements = document.QuerySelectorAll("img");
+            foreach (var element in elements)
+            {
+                var src = element.Attributes["src"].Value;
+                element.SetAttribute("src", "loading.gif");
+                element.SetAttribute("data-src", src);
+            }
+
+            Console.WriteLine(document.Body.InnerHtml);
+
+
+            Console.ReadKey();
+        }
+    }
+}
